48df75b0c08bce4594b0331e9b3fdcafa37efccb
ivan3582004 <ivans05251998@gmail.com>
2020-05-09 17:41:54 +0800

    Update ticketStatistics.php

--- ticketStatistics.php
+++ ticketStatistics.php
@@ -1,5 +1,5 @@
 <?php require'base/header.php';?>
-Not finish
+<h1>Chart not finish</h1>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
 
 <div class="container">
@@ -45,6 +45,35 @@ Not finish
 		}
 	});
 	</script>
+
+	<?php
+		require("Modle/conn.php");
+		echo 'Shop ABC<br>';
+		for($i = 0; $i < 7; $i++){
+			$date = date("Y-m-d",strtotime("-" . $i . " days"));
+			if ($result = mysqli_query($conn, "SELECT * FROM ticket WHERE ShopID = '1' AND Date = '" . $date . "'")) {
+				echo $date . ": " . mysqli_num_rows($result) . " ticket<br>";
+				mysqli_free_result($result);
+			}
+		}
+		echo 'Shop BBC<br>';
+		for($i = 0; $i < 7; $i++){
+			$date = date("Y-m-d",strtotime("-" . $i . " days"));
+			if ($result = mysqli_query($conn, "SELECT * FROM ticket WHERE ShopID = '2' AND Date = '" . $date . "'")) {
+				echo $date . ": " . mysqli_num_rows($result) . " ticket<br>";
+				mysqli_free_result($result);
+			}
+		}
+		echo 'Shop CBC<br>';
+		for($i = 0; $i < 7; $i++){
+			$date = date("Y-m-d",strtotime("-" . $i . " days"));
+			if ($result = mysqli_query($conn, "SELECT * FROM ticket WHERE ShopID = '3' AND Date = '" . $date . "'")) {
+				echo $date . ": " . mysqli_num_rows($result) . " ticket<br>";
+				mysqli_free_result($result);
+			}
+		}
+		mysqli_close($conn);
+	?>
 
 </div>
 <?php require'base/footer.php';?>
